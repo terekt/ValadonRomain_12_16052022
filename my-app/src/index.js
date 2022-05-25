cf63dce9e2950d081cd4861501c86027a19fec79
Romainv78 <romainvaladon@gmail.com>
2022-05-25 15:02:51 +0200

    rechart + 1er data viz

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profil from "./pages/profil/profil";
-import Page404 from './pages/page404/page404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,6 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Profil />} />
-        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- my-app/src/pages/profil/profil.js
+++ my-app/src/pages/profil/profil.js
@@ -6,29 +6,45 @@ import Time from "../../components/Time/Time";
 import Rating from "../../components/Rating/Rating";
 import Score from "../../components/Score/Score";
 import Nutrition from "../../components/Nutrition/Nutrition";
+import Data from "../../assets/data";
 
 function Profil() {
+
+    var Main_data = Data.USER_MAIN_DATA[0];
+    //console.log(Main_data);
+    var Activity = Data.USER_ACTIVITY[0];
+    //console.log(Activity);
+    var Average_session = Data.USER_AVERAGE_SESSIONS[0];
+    //console.log(Average_session);
+    var Performance = Data.USER_PERFORMANCE[0];
+    //console.log(Performance);
+
+
+
+
     return (
         <>
             <div className="Home">
                 <Hnavbar />
                 <div className="dashboard">
                     <Vnavbar />
-                    <div className="welcome">
-                        <div>Bonjour</div>
-                        <div>Thomas</div>
-                        <div>Félicitation ! Vous avez explosé vos objectifs hier 👏</div>
-                    </div>
-                    <div className="left-container">
-                        <Daily />
-                        <div className="bottom-widgets">
-                            <Time />
-                            <Rating />
-                            <Score />
+                    <div className="content-wrap">
+                        <div className="welcome">
+                            <div>Bonjour</div>
+                            <div>{Main_data.userInfos.firstName}</div>
+                            <div>Félicitation ! Vous avez explosé vos objectifs hier 👏</div>
                         </div>
-                    </div>
-                    <div className="right-container">
-                        <Nutrition />
+                        <div className="left-container">
+                            <Daily data={Activity} />
+                            <div className="bottom-widgets">
+                                <Time data={Average_session} />
+                                <Rating data={Performance} />
+                                <Score data={Main_data.todayScore} />
+                            </div>
+                        </div>
+                        <div className="right-container">
+                            <Nutrition data={Main_data.keyData} />
+                        </div>
                     </div>
                 </div>
             </div>
